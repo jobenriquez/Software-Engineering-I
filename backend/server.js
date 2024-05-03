3ec7b56b3393a8a5ae38d2c408c79d2ea7ae9fff
Laacertaa <aldrinlizardoa@gmail.com>
2024-05-03 14:54:44 +0800

    added error handling branch for fixing socket implementation

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -26,7 +26,7 @@ app.use("/api/users", userRoutes);
 
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server running on port ${PORT}`);
 });
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,6 +3,7 @@ import { TiMessages } from "react-icons/ti";
 import Messages from './Messages';
 import MessageInput from './MessageInput';
 import useConversation from '../../zustand/useConversation';
+import { useAuthContext } from '../../context/AuthContext';
 
 const MessageContainer = () => {
   const {selectedConversation, setSelectedConversation} = useConversation();
@@ -36,10 +37,12 @@ const MessageContainer = () => {
 export default MessageContainer;
 
 const NoChatSelected = () => {
+  const {authUser} = useAuthContext();
+
   return (
     <div className='flex items-center justify-center w-full h-full'>
       <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-        <p> WELCOME, JUAN DELA CRUZ!</p>
+        <p> Welcome, {authUser.fullName}!</p>
         <p> Select a chat to start messaging</p>
         <TiMessages className='text-3xl md:text-6xl text-center'/>
       </div>
