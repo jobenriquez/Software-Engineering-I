7283b55f6fb28e986007c98e98d967201f5c3328
Laacertaa <aldrinlizardoa@gmail.com>
2024-05-16 15:31:35 +0800

    updated MessageContainer.jsx to show user's name instead of generic name

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
+import useConversation from "../../zustand/useConversation";
+import MessageInput from "./MessageInput";
+import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
-import Messages from './Messages';
-import MessageInput from './MessageInput';
-import useConversation from '../../zustand/useConversation';
+import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
   const {selectedConversation, setSelectedConversation} = useConversation();
@@ -36,10 +37,11 @@ const MessageContainer = () => {
 export default MessageContainer;
 
 const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
   return (
     <div className='flex items-center justify-center w-full h-full'>
       <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-        <p> WELCOME, JUAN DELA CRUZ!</p>
+        <p> Welcome, {authUser.fullName}!</p>
         <p> Select a chat to start messaging</p>
         <TiMessages className='text-3xl md:text-6xl text-center'/>
       </div>
